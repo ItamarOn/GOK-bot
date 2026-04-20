4d3c5bd0107a241141a5861d7d52fe13f68553f2
ItamarOn <itamar.tech.projects@gmail.com>
2026-04-20 23:53:53 +0300

    readability (for Grafana)

--- utils/thin_log.py
+++ utils/thin_log.py
@@ -1,15 +1,31 @@
 import copy
 from config import logger
 
+def _redact_sensitive_data(data, target_key="jpegThumbnail", replacement="..."):
+    """
+    Recursively traverse a dictionary or list to replace target_key values.
+    """
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if key == target_key:
+                data[key] = replacement
+            else:
+                _redact_sensitive_data(value, target_key, replacement)
+    elif isinstance(data, list):
+        for item in data:
+            _redact_sensitive_data(item, target_key, replacement)
+    return data
+
+
 def thin_log(whatsapp_request):
     log_view = copy.deepcopy(whatsapp_request)
-    try:
-        log_view['messageData']['fileMessageData']['jpegThumbnail'] = "..."
-    except (KeyError, TypeError):
-        pass
-    try:
-        log_view['messageData']['extendedTextMessageData']['jpegThumbnail'] = "..."
-    except (KeyError, TypeError):
-        pass
+    _redact_sensitive_data(log_view)
 
-    logger.info(f"Request: {log_view}")
+    sender_name = whatsapp_request.get('senderData', {}).get('senderName')
+    download_url = whatsapp_request.get('messageData', {}).get('fileMessageData', {}).get('downloadUrl')
+
+    full_log = f"Request: {log_view}"
+    if sender_name and download_url:
+        full_log += f"\nSender: {sender_name}\nURL: {download_url}"
+
+    logger.info(full_log)
